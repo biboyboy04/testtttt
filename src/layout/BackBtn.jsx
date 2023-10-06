17f7f7cc0531134c253ae491cd68aad550f59090
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-10-07 06:39:14 +0800

    Update BackBtn.jsx
    Add repo name as link to go to the home page

--- src/layout/BackBtn.jsx
+++ src/layout/BackBtn.jsx
@@ -1,13 +1,13 @@
-import {BsBoxArrowLeft} from 'react-icons/bs'
+import { BsBoxArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const BackBtn = () => {
   return (
-    <Link to={'/'}>
+    <Link to={"/testtttt/"}>
       <div className="backBtncontainer">
-             <BsBoxArrowLeft className='backBtn'></BsBoxArrowLeft>
-          </div>
+        <BsBoxArrowLeft className="backBtn"></BsBoxArrowLeft>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default BackBtn
+export default BackBtn;
